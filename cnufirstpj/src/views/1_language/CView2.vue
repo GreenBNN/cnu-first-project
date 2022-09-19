04686ddf03a8b0ab297b3444b9339bfbfb006f88
CornBee <cornbee80014@gmail.com>
2022-09-19 17:48:17 +0900

    update R-wordArr when input is done, css to align items

--- cnufirstpj/src/views/1_language/CView2.vue
+++ cnufirstpj/src/views/1_language/CView2.vue
@@ -1,20 +1,20 @@
 <template>
-  <!-- 이제 다 입력하면 새로 랜덤하게 불러오는 기능 만들면 됨. -->
   <div>
-    <div :key="index" v-for="(word, index) in RandSelectedWordArr">
-      <div>
-        {{ word.value }}
-        <input
-          @keyup.enter="index = gotoNextInput(index)"
-          type="text"
-          v-focus="focusBool"
-          :class="{ hideInput: hideBool[index] }"
-          v-model="typedArr[index]"
-        />
+    <div><h1>코딩 타자 연습</h1></div>
+    <div :class="{ flex: true, rowDirectionItems: true, centerItems: true }">
+      <div :key="index" v-for="(word, index) in RandSelectedWordArr">
+        <div :class="{ columnDirectionItems: true }">
+          {{ word.value }}
+          <br />
+          <br />
+          <input
+            @keyup.enter="index = gotoNextInput(index)"
+            type="text"
+            v-focus
+            :class="{ hideInput: hideBool[index] }"
+          />
+        </div>
       </div>
-    </div>
-    <div>
-      <button @click="testf(index)">파라미터 값 보기</button>
     </div>
   </div>
 </template>
@@ -31,7 +31,6 @@ export default {
   directives: {
     focus
   },
-  name: '',
   components: {},
   data() {
     return {
@@ -39,7 +38,6 @@ export default {
       selectedWordIndex: 0,
       RandSelectedWordArr: [],
       hideBool: [],
-      focusBool: true,
 
       CwordArr: [
         // C 언어에서 사용하는 단어들 배열
@@ -50,10 +48,13 @@ export default {
         { value: 'pipe', originidx: 4 },
         { value: 'man', originidx: 5 },
         { value: 'because', originidx: 6 },
-        { value: 'xor', originidx: 7 }
-      ],
-
-      typedArr: []
+        { value: 'xor', originidx: 8 },
+        { value: 'cannot', originidx: 9 },
+        { value: 'nono', originidx: 10 },
+        { value: ';', originidx: 11 },
+        { value: 'disjunction', originidx: 12 },
+        { value: 'conjunction', originidx: 13 }
+      ]
     }
   },
   setup() {},
@@ -81,31 +82,43 @@ export default {
       // ...은 전개 연산자로 넣지 않으면
       const RandSelectedWordArr = shuffled.slice(0, num)
       // 그렇게 랜덤하게 정렬된 배열을 num으로 지정한 길이만큼 잘라냄
-      console.log(shuffled)
-      console.log(RandSelectedWordArr)
       return RandSelectedWordArr
     },
     gotoNextInput(index) {
-      this.changeNowNextBool(index)
+      this.visibleNextOf(index)
+      if (index + 1 === this.numberToSelect) {
+        // index가 끝에 다다르면 새로 RandSelect 함.
+        this.RandSelectedWordArr = this.RandSelectFunction(
+          this.CwordArr,
+          this.numberToSelect
+        )
+        this.visibleNextOf(-1)
+        // index가 0인 input 칸이 보이도록 함
+        return 0
+      }
       return index + 1
     },
-    changeNowNextBool(index) {
+    visibleNextOf(index) {
       this.hideBool[index] = true
       this.hideBool[index + 1] = false
-    },
-
-    testf(toSee) {
-      console.log(this.toSee)
-    },
-    focusOn() {
-      this.$refs.input1.focus()
-      // template에서 ref 태그로 input이라고 이름 지어준 곳에 포커스를 맞춤.
     }
   }
 }
 </script>
 <style scoped>
+.flex {
+  display: flex;
+}
 .hideInput {
   visibility: hidden;
+}
+.columnDirectionItems {
+  flex-direction: column;
+}
+.rowDirectionItems {
+  flex-direction: row;
+}
+.centerItems {
+  justify-content: center;
 }
 </style>
